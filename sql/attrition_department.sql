ae2086cc5e5a7b4b0aa92a27eea62809efea05eb
aminahharp <aminahgrace14@gmail.com>
2025-08-24 23:21:26 -0400

    Update attrition_department.sql

--- sql/attrition_department.sql
+++ sql/attrition_department.sql
@@ -1,10 +1,11 @@
 SELECT 
-    jobrole,
+    department,
     COUNT(*) AS total_people,
     SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,
     ROUND(100.0 * AVG(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END), 2) AS attrition_pct
 FROM hr.employee_raw
-GROUP BY jobrole
+GROUP BY department
 ORDER BY attrition_pct DESC;
+
 
 
