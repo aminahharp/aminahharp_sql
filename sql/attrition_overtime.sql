34e0f0ec5fe79c2442052c65d7cf65b4b7dfe731
aminahharp <aminahgrace14@gmail.com>
2025-08-24 23:21:47 -0400

    Update attrition_overtime.sql

--- sql/attrition_overtime.sql
+++ sql/attrition_overtime.sql
@@ -1,10 +1,9 @@
-SELECT 
-    jobrole,
-    COUNT(*) AS total_people,
-    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,
-    ROUND(100.0 * AVG(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END), 2) AS attrition_pct
+SELECT
+  overtime,
+  COUNT(*) AS people,
+  ROUND(100.0 * AVG(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END), 2) AS attrition_pct
 FROM hr.employee_raw
-GROUP BY jobrole
+GROUP BY overtime
 ORDER BY attrition_pct DESC;
 
 
