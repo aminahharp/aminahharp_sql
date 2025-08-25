e32294b29498bd1f457be741619916c28d1f2691
aminahharp <aminahgrace14@gmail.com>
2025-08-24 23:17:02 -0400

    Update total_employees.sql

--- sql/total_employees.sql
+++ sql/total_employees.sql
@@ -1,10 +1,3 @@
-SELECT 
-    jobrole,
-    COUNT(*) AS total_people,
-    SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS attrition_count,
-    ROUND(100.0 * AVG(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END), 2) AS attrition_pct
-FROM hr.employee_raw
-GROUP BY jobrole
-ORDER BY attrition_pct DESC;
+SELECT COUNT(*) AS employees FROM hr.employee_raw;
 
 
